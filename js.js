b91b0ca8f305ccadcc37acff822051894082ed07
keepcalling <keepcalling@admins-Air-10.lan>
2017-10-16 16:28:25 +0300

    First Commit

---
+++ js.js
@@ -0,0 +1,17 @@
+// Sticky Nav
+
+const nav = document.querySelector('nav#main');
+const topOfNav = nav.offsetTop;
+
+function stickyNav() {
+	if (window.scrollY > topOfNav) {
+		document.body.style.paddingTop = nav.offsetHeight + 'px';
+		document.body.classList.add('fixed-nav');
+	} else {
+		document.body.style.paddingTop = 0;
+		document.body.classList.remove('fixed-nav');
+	}
+}
+
+window.addEventListener('scroll', stickyNav);
+
